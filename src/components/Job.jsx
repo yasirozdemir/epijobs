cb38d81f7fcdb229987770a5e00954909946a70e
Muhammed Yasir Özdemir <yasiryksmail@gmail.com>
2023-02-09 17:09:56 +0300

    update before starting to work on redux implementation

--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -1,26 +1,34 @@
 import { Row, Col, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { BsStarFill } from "react-icons/bs";
+import { AiTwotoneLike, AiFillDislike } from "react-icons/ai";
 
 const Job = ({ data }) => (
   <Row
-    className="mx-0 mt-3 p-3"
+    className="jobCard align-items-center mx-0 mt-3 p-3"
     style={{ border: "1px solid #00000033", borderRadius: 4 }}
   >
     <Col xs={3}>
-      <Link to={`/${data.company_name}`}>{data.company_name}</Link>
+      <Link to={`/${data.company_name}`} className="text-dark">
+        {data.company_name}
+      </Link>
     </Col>
     <Col>
-      <a href={data.url} target="_blank" rel="noreferrer">
+      <a href={data.url} target="_blank" rel="noreferrer" className="text-dark">
         {data.title}
       </a>
     </Col>
     <Col xs={1}>
       <Button
-        variant="outline-dark"
+        variant="outline-success"
         className="d-flex justify-content-center align-items-center"
       >
-        <BsStarFill />
+        <AiTwotoneLike />
+      </Button>
+      <Button
+        variant="outline-danger"
+        className="d-flex justify-content-center align-items-center"
+      >
+        <AiFillDislike />
       </Button>
     </Col>
   </Row>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import "./style/style.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<App />)
+root.render(<App />);
