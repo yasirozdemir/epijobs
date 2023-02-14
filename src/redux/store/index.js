247b2ef6787769e41904c2e4a52f0365bbe12d81
Muhammed Yasir Özdemir <yasiryksmail@gmail.com>
2023-02-14 16:54:14 +0300

    store update

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -2,13 +2,42 @@ import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import CompanyReducer from "../reducers/CompanyReducer";
 import FavReducer from "../reducers/FavReducer";
 import JobReducer from "../reducers/JobReducer";
+import { persistStore, persistReducer } from "redux-persist";
+import localStorage from "redux-persist/es/storage";
+import { encryptTransform } from "redux-persist-transform-encrypt";
 
-const store = configureStore({
-  reducer: combineReducers({
-    favorites: FavReducer,
-    job: JobReducer,
-    company: CompanyReducer,
-  }),
+const persistConfig = {
+  storage: localStorage,
+  key: "root",
+  // this brings the whole redux store into persistency
+  transforms: [
+    encryptTransform({
+      secretKey: process.env.REACT_APP_ENCRYPTION_KEY,
+    }),
+  ],
+};
+
+const combinedReducers = combineReducers({
+  favorites: FavReducer,
+  job: JobReducer,
+  company: CompanyReducer,
 });
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, combinedReducers);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  // ...giving back to reducer a single function once again
+  // we're telling Redux which reducer function to use!
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware({
+      serializableCheck: false,
+      // this shuts off the checking of non-serializable values in actions
+    });
+  },
+});
+
+const persistedStore = persistStore(store);
+// a persisted version of our store
+
+export { store, persistedStore };
